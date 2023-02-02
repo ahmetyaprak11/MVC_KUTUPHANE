a79f6cd6e75000e25b5ce314d6ff8b9ccb305f26
ahmetyaprak11 <ahmet.yaprak96@hotmail.com>
2023-02-02 23:01:55 +0300

    Kategori Ekleme işlemleri yapıldı. "KategoriEkle" dosyası oluşturuldu.

--- MVC_KUTUPHANE/Controllers/KategoriController.cs
+++ MVC_KUTUPHANE/Controllers/KategoriController.cs
@@ -16,5 +16,11 @@ namespace MVC_KUTUPHANE.Controllers
             var degerler = db.TBLKATEGORI.ToList(); //Kategori tablomuzdaki değerleri bize liste şeklinde getirmeyi sağladık. "degerler" adlı değişkene atadık. 
             return View(degerler); // Daha sonra bu "degerler" i bize döndürmesini istedik.
         }
+        public ActionResult KategoriEkle(TBLKATEGORI p)  //ActionResult ile metod oluşturup "View" sayfası ile ilişkilendirip işlem yapıyoruz. Bu metod bir parametre alacak. Bu parametre hangi tablo ile işlem yapacaksa onu yazıyoruz. "p" dediğimiz yere istediğimizi yazabiliyoruz.
+        {
+            db.TBLKATEGORI.Add(p); //"p" den gelecek olan değerleri ekleyecek. Bu değerleri eklemenin "View" sayfasından yakalayacağız.
+            db.SaveChanges();
+            return View();
+        }
     }
 }
