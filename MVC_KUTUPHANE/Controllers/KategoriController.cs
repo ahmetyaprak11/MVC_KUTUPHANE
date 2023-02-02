fa16501cfb6615b8c362231e4e687ae4c3acc209
ahmetyaprak11 <ahmet.yaprak96@hotmail.com>
2023-02-02 21:51:40 +0300

    KategoriController dosyasında yeni bir db değişkeni oluşturuldu. Daha sonra tablo verileri liste şeklinde alındı.

--- MVC_KUTUPHANE/Controllers/KategoriController.cs
+++ MVC_KUTUPHANE/Controllers/KategoriController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_KUTUPHANE.Models.Entity; //SQL sorgularını bizim için yapacak olan Entity Framework'unu tanımladık.
 
 namespace MVC_KUTUPHANE.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities(); // Modelimize ait yeni bir nesne oluşturuyoruz. Bunu db adlı değişkene atıyoruz. Tablomuza ulaşmamızı sağlıyor.   
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TBLKATEGORI.ToList(); //Kategori tablomuzdaki değerleri bize liste şeklinde getirmeyi sağladık. "degerler" adlı değişkene atadık. 
+            return View(degerler); // Daha sonra bu "degerler" i bize döndürmesini istedik.
         }
     }
 }
