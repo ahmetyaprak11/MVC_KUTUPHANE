a1a0a7d27e2b8aaa084082a71dcf3226765fff3e
ahmetyaprak11 <ahmet.yaprak96@hotmail.com>
2023-02-03 23:54:59 +0300

    Personel ekleme, silme ve güncelleme yapıldı.

---
+++ MVC_KUTUPHANE/Controllers/PersonelController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_KUTUPHANE.Models.Entity;
+namespace MVC_KUTUPHANE.Controllers
+{
+    public class PersonelController : Controller
+    {
+        // GET: Personel
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLPERSONEL.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult PersonelEkle() //bunu yapmazsak safyayı çağırdığımızda db ye boş bir ekleme yapıyor.
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult PersonelEkle(TBLPERSONEL p)  //ActionResult ile metod oluşturup "View" sayfası ile ilişkilendirip işlem yapıyoruz. Bu metod bir parametre alacak. Bu parametre hangi tablo ile işlem yapacaksa onu yazıyoruz. "p" dediğimiz yere istediğimizi yazabiliyoruz.
+        {
+            if(!ModelState.IsValid)
+            {
+                return View("PersonelEkle");
+            }
+
+            db.TBLPERSONEL.Add(p); //"p" den gelecek olan değerleri ekleyecek. Bu değerleri eklemenin "View" sayfasından yakalayacağız.
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult PersonelSil(int id)
+        {
+            var person = db.TBLPERSONEL.Find(id);
+            db.TBLPERSONEL.Remove(person);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult PersonelGetir(int id)
+        {
+            var prs = db.TBLPERSONEL.Find(id);
+            return View("PersonelGetir", prs);
+        }
+        public ActionResult PersonelGuncelle(TBLPERSONEL p)
+        {
+            var prs = db.TBLPERSONEL.Find(p.ID);
+            prs.PERSONEL = p.PERSONEL;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
