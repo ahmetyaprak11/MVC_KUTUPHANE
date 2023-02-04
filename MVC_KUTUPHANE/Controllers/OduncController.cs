099b5a0e9c38631af9426bea7bd10efa07eef967
ahmetyaprak11 <ahmet.yaprak96@hotmail.com>
2023-02-04 21:10:43 +0300

    Ödünç verme, iade alma işlemleri yapıldı. Ödünç verilen kitabın sistemde durumu false oldu. Böylelikle ödünç verilen kitap tekrar verilemez oldu.

---
+++ MVC_KUTUPHANE/Controllers/OduncController.cs
@@ -0,0 +1,32 @@
+﻿using MVC_KUTUPHANE.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_KUTUPHANE.Models.Entity;
+
+namespace MVC_KUTUPHANE.Controllers
+{
+    public class OduncController : Controller
+    {
+        // GET: Odunc
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult OduncVer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KategoriEkle(TBLHAREKET p) 
+        {
+            db.TBLHAREKET.Add(p);
+            db.SaveChanges();  
+            return View();
+        }
+    }
+}
