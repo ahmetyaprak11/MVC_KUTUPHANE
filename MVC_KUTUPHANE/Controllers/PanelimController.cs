052cc10801baef4d2964137c686117f6e0268866
ahmetyaprak11 <ahmet.yaprak96@hotmail.com>
2023-02-09 13:00:23 +0300

    Login işlemi gerçekleştirildi.

---
+++ MVC_KUTUPHANE/Controllers/PanelimController.cs
@@ -0,0 +1,37 @@
+﻿using MVC_KUTUPHANE.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC_KUTUPHANE.Controllers
+{
+    public class PanelimController : Controller
+    {
+        // GET: Panelim
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var uyemail = (string)Session["Mail"];
+            var degerler = db.TBLUYELER.FirstOrDefault(z => z.MAIL == uyemail);
+            return View(degerler);
+        }
+        [HttpPost]
+        public ActionResult Index2(TBLUYELER p)
+        {
+            var kullanici = (string)Session["Mail"];
+            var uye = db.TBLUYELER.FirstOrDefault(x => x.MAIL == kullanici);
+            uye.SIFRE = p.SIFRE;
+            uye.AD = p.AD;
+            uye.FOTOGRAF = p.FOTOGRAF;
+            uye.OKUL = p.OKUL;
+            uye.KULLANICIADI = p.KULLANICIADI;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
